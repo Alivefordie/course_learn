3452928cba75a0231964b163a1542b660f8d304d
Kittanawat Wattanakit <kittanawat0916@gmail.com>
2024-02-17 00:52:50 +0700

    create register form ui

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,9 +2,12 @@ import React from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+    const navigate = useNavigate();
     const initialUser = { email: "", password: "", username: "" };
     const [user, setUser] = useState(initialUser);
     const registerUser = async () => {
@@ -18,6 +21,7 @@ const Register = () => {
             console.log('Well done!');
             console.log('User profile', response.data.user);
             console.log('User token', response.data.jwt);
+            navigate('/Page1');
         } catch (error) {
           
             console.log('An error occurred:', error.response);
@@ -35,41 +39,26 @@ const Register = () => {
         }));
       };
     return (
-        <div>
-            <h1>
-            <div>
-          <h2 >Sign up:</h2>
-          <Form>
-            <input
-              type="text"
-              name="username"
-              value={user.username}
-              onChange={handleUserChange}
-              placeholder="Please Enter Username"
-            />
-          </Form>
-          <Form>
-            <input
-              type="email"
-              name="email"
-              value={user.email}
-              onChange={handleUserChange}
-              placeholder="Enter your email"
-            />
-          </Form>
-          <Form>
-            <input
-              type="password"
-              name="password"
-              value={user.password}
-              onChange={handleUserChange}
-              placeholder="Enter password"
-            />
-          </Form>
-        </div>
-                <button variant="outline-dark" onClick={handleRegisterClick}>Register</button>
-            </h1>
-        </div>
+        <div className="container"> 
+      <h1>Register</h1>
+      <Form className="rounded p-4 border shadow">
+        <Form.Group controlId="name" >
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter your full name" name = "username" value = {user.username} onChange={handleUserChange}/>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" name = "email" value = {user.email} onChange={handleUserChange}/>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter password" name = "email" value = {user.password} onChange={handleUserChange}/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+    </div>
     );
 };
 
