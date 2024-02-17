0cf3a874ebd2a0d72a91e0ddd95708eaab4f6c03
Chaithanan <chaithanan.pk5199@gmail.com>
2024-02-17 16:05:54 +0700

    Login Form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,15 +1,76 @@
 import React from "react";
-// import axios from "axios";
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const initialUser = {  password: "", username: "" };
+    const [user, setUser] = useState(initialUser);
+    const [submitEnabled, setSubmitEnabled] = useState(true);
 
+    const handleUserChange = ({ target }) => {
+        const { id, value } = target;
+        setUser((currentUser) => ({
+          ...currentUser,
+          [id]: value,
+        }));
+      };
 
-
+    const handleSubmit = async (e) => {
+        console.log('submitted');
+        e.preventDefault();
+        setSubmitEnabled(false);
+        try{
+            let response = await axios.post('http://localhost:1337/api/auth/local', {
+                identifier: user.username,
+                password: user.password,
+            });
+            console.log(response);
+            if (response.data){
+            navigate('/Page1');}
+            
+        } catch (error){
+            console.log(error);
+            console.log('wrong username & password');
+            setSubmitEnabled(true);
+        }
+    }
 
     return (
+        <div className="container"> 
+        <Form onSubmit={handleSubmit} className="rounded p-4 border shadow">
+        <h2>Sign In</h2>
+        <Form.Group controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+                type="text"
+                placeholder="Enter username"
+                value={user.username}
+                onChange={handleUserChange}
+                required
+            />
+        </Form.Group>
+
+        <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                type="password"
+                placeholder="Password"
+                value={user.password}
+                onChange={handleUserChange}
+                required
+            />
+        </Form.Group>
+
         <div>
-            <h1>Login</h1>
+            <Button variant="primary" type="submit" disabled={!submitEnabled} >
+                Submit
+            </Button>
         </div>
+    </Form>
+    </div>
     );
 };
 
