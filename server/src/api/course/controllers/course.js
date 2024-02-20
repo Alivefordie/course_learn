3888146fcf8994be892b99e7710017ff4035a09f
Khomsan Khongpon <the78639@gmail.com>
2024-02-20 08:18:33 +0700

    get favorite courses api

--- server/src/api/course/controllers/course.js
+++ server/src/api/course/controllers/course.js
@@ -6,5 +6,17 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::course.course", ({ strapi }) => ({
-  async favorite(ctx) {},
+  async favorite(ctx) {
+    const entries = await strapi.db.query("api::course.course").findMany({
+      where: {
+        entries: {
+          like: {
+            $notNull: true,
+          },
+        },
+      },
+    });
+    const sanitizedResults = await this.sanitizeOutput(entries, ctx);
+    return this.transformResponse(sanitizedResults);
+  },
 }));
