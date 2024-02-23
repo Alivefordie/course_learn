e4a1fff49b02019d116fab4ce510e46a2e58d433
Jukra <jukrachai3146@gmail.com>
2024-02-24 02:44:19 +0700

    mycart change layout and button

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,14 +1,21 @@
 import React from "react";
-const Cart = () =>{
-
-
-    return(
-        <div>
-            <h1>
-                Cart
-            </h1>
+import NavbarTop from "../components/NavbarTop";
+import { Button, Container, Row } from "react-bootstrap";
+const Cart = () => {
+  return (
+    <div>
+      <NavbarTop />
+      <Container className="cart-container">
+        <h1 style={{ textAlign: "center" }}>myCart</h1>
+        <Container className="incart-container">
+          <h2>incart</h2>
+        </Container>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button variant="outline-dark">Payment</Button>
         </div>
-    )
-}
+      </Container>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
