b72242ae38c1ddc136b031507c02068e394a585b
Jukra <jukrachai3146@gmail.com>
2024-02-24 02:48:09 +0700

    myCart add Navbar

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,16 +1,18 @@
 import React from "react";
 import NavbarTop from "../components/NavbarTop";
-import { Button, Container, Row } from "react-bootstrap";
+import NavbarLink from "../components/NavbarLink";
+import { Button, Container } from "react-bootstrap";
+
 const Cart = () => {
   return (
     <div>
-      <NavbarTop />
+      <NavbarTop NavbarLink={NavbarLink} />
       <Container className="cart-container">
         <h1 style={{ textAlign: "center" }}>myCart</h1>
         <Container className="incart-container">
           <h2>incart</h2>
         </Container>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ display: "flex", justifyContent: "center" }}>
           <Button variant="outline-dark">Payment</Button>
         </div>
       </Container>
