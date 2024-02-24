466ecea8a0503a6308cbcbe2463c1ed468551ae8
Jukra <jukrachai3146@gmail.com>
2024-02-25 03:32:40 +0700

    change cart form

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,21 +1,39 @@
 import React from "react";
 import NavbarTop from "../components/NavbarTop";
 import NavbarLink from "../components/NavbarLink";
-import { Button, Container } from "react-bootstrap";
-import '../App.css'
+import { Button, Col, Container, Row } from "react-bootstrap";
+import "../App.css";
 
 const Cart = () => {
   return (
     <div className="body">
-<NavbarTop NavbarLink={NavbarLink} />
-      <Container className="cart-container" >
-        <h1 style={{ textAlign: "center" }}>myCart</h1>
+      <NavbarTop NavbarLink={NavbarLink} />
+      <Container className="cart-container">
         <Container className="incart-container">
-          <h2>incart</h2>
-        </Container>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Button variant="outline-dark">Payment</Button>
+          <h1 style={{ textAlign: "center" }}>myCart</h1>
+          <Row>
+          <Col className="order-col" sm={8}>
+            <p>Order</p>
+            <Container className="item-cart">
+              <p>
+                item 1
+              </p>
+            </Container>
+          </Col>
+          <Col className="price-col" sm={2}>
+            <p>Price</p>
+            <Container className="price-cart">
+              <p>
+                2,990
+              </p>
+            </Container>
+          </Col>
+          </Row>
+          <div style={{ display: "flex", justifyContent: "center"}}>
+          <Button variant="outline-dark" className="pay-but">Payment</Button>
         </div>
+        </Container>
+        
       </Container>
     </div>
   );
