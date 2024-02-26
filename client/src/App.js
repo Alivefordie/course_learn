e473774949199f128f98c9320a9fa3622ccf0b7c
Khomsan Khongpon <the78639@gmail.com>
2024-02-27 00:52:17 +0700

    fix body css

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
 import "./App.css";
-import NavbarTop from "./components/NavbarTop";
-import NavbarLink from "./components/NavbarLink";
-import { BsBell } from "react-icons/bs";
 import React, { useEffect, useState } from "react";
 import AllCourse from "./pages/allCourse";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div className="body">
-      <Login/>
-    </div>
-  );
+	return (
+		<div className="body">
+			<AllCourse />
+		</div>
+	);
 }
 
 export default App;
