5b75e0ea30f1ca069e383b35e3052daac31072ca
Kittanawat Wattanakit <kittanawat0916@gmail.com>
2024-02-26 22:42:25 +0700

    UI-Register

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import NavbarLink from "./components/NavbarLink";
 import { BsBell } from "react-icons/bs";
 import React, { useEffect, useState } from "react";
 import AllCourse from "./pages/allCourse";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="body">
-      <AllCourse />
+      <Register/>
     </div>
   );
 }
