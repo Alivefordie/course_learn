fdb482a06a0182eb2150e6609ba9d81316d9f0d2
Jukra <jukrachai3146@gmail.com>
2024-02-28 01:05:24 +0700

    newest data

---
+++ client/src/components/Newest.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+
+const Newest = ({ data }) => {
+  useEffect(() => {
+    console.log("Common", data);
+  });
+
+  return (
+    <div>
+      {data
+        .sort((a, b) => new Date(b.lastDate) - new Date(a.lastDate))
+        .slice(0, 2)
+        .map((item) => (
+          <Card key={item.id} style={{ width: "100%" }} className="d-flex flex-row mb-3">
+            <div className="image-col">
+              <Card.Img
+                className="course-image"
+                variant="left"
+                src={
+                  "http://localhost:1337" +
+                  item.attributes.picture.data.attributes.url
+                }
+                alt="item"
+                width={50}
+              />
+            </div>
+            <div className="body-col">
+              <Card.Body>
+                <Card.Title>{item.attributes.title}</Card.Title>
+                <Card.Text>{item.attributes.description}</Card.Text>
+              </Card.Body>
+              <Col className="add-icon position-absolute top-0 end-0 p-2">
+                <img
+                  src="../plus.png"
+                  style={{ width: "20px", height: "20px" }}
+                  alt="Add Icon"
+                />
+              </Col>
+              <Col className="heart-icon position-absolute bottom-0 end-0 p-2">
+                <img
+                  src="../heart.png"
+                  style={{ width: "20px", height: "20px" }}
+                  alt="Like Icon"
+                />
+              </Col>
+              <Link to={{ pathname: `/courses/${item.id}` }}>
+                <Button variant="outline-dark">Detail</Button>
+              </Link>
+            </div>
+          </Card>
+        ))}
+    </div>
+  );
+};
+
+export default Newest;
