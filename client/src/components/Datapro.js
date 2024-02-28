c2e9a61efb055486b4398334695ad88f267d3364
Pongphisut Thepthong <oookung75@gmail.com>
2024-02-29 00:58:04 +0700

    profile

--- client/src/components/Datapro.js
+++ client/src/components/Datapro.js
@@ -1,9 +1,18 @@
-import React  from "react";
-
-const Datapro = () =>{
+import React, { useEffect }  from "react";
 
 
+const Datapro = ({data}) =>{
+    
+    const data_1 = data[0];
+    const entries = data[1]
+    const course = data[2]
 
+
+    useEffect(() => {
+        console.log("data:",data_1)
+        console.log("entries",entries)
+        console.log("course",course)
+    }, [data_1,entries,course])
 
 
     return(
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,30 +1,43 @@
 import React, { useEffect, useState } from "react";
-// import Datapro from "../components/Datapro";
+import Datapro from "../components/Datapro";
 // import axios from "axios";
 import conf from "../conf/main";
 import ax from "../conf/ax";
+import NavbarTop from "../components/NavbarTop";
+import NavbarLink from "../components/NavbarLink";
 
-const Profile = () =>{
-const [data,setdata] = useState({})
-    const fetchProfile = async() =>{ 
-        try{
-            const response = await ax.get(conf.Profile); 
-            console.log(response.data); 
-            setdata(response.data)
+const Profile = () => {
+    const [data, setdata] = useState({});
+    const [entries, setentries] = useState([]);
+    const [course, setcourse] = useState([]);
+
+    const fetchProfile = async () => {
+        try {
+            const response = await ax.get(conf.test);
+            setdata(response.data);
+            // console.log("response data: ", response.data);
+
+            setentries(response.data.entries);
+            // console.log("response entries: ", response.data.entries);
+
+            
+            const courses = response.data.entries.map((item) => item.course);
+            setcourse(courses);
+            // console.log("response course: ", courses);
         }
-        catch(error){
+        catch (error) {
             console.log("fail");
         }
     };
 
-    useEffect(()=>{
-        fetchProfile(); 
-    },[]);
+    useEffect(() => {
+        fetchProfile();
+    }, []);
 
-    return(
+    return (
         <div>
-            profile132
-            <p>username:{data.username}</p>
+            <NavbarTop NavbarLink={NavbarLink} />
+            <Datapro data={[data, entries, course]} />
         </div>
     );
 };
