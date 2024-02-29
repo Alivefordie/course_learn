6563b28518cb39bb5d0dd315dc9267a0ce421414
Chaithanan <chaithanan.pk5199@gmail.com>
2024-03-01 01:11:31 +0700

    profileLink

--- client/src/components/NavbarLink.js
+++ client/src/components/NavbarLink.js
@@ -1,32 +1,38 @@
 import { Nav } from "react-bootstrap";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import styles from "../css/Navbar.module.css";
+
 const NavbarLink = () => {
 	const context = useContext(AuthContext);
 	return (
 		<Nav className="NavbarLink" style={{ marginLeft: "auto", marginRight: "10px" }}>
-			<Nav.Link href="/mycourses" style={{ flexDirection: "column", textAlign: "center" }}>
-				<img src="../course.png" style={{ width: "40px", height: "40px" }} />
+			<Nav.Link href="/mycourses" className={styles.text}>
+				<img src="../course.png" className={styles.picture} />
 				<div style={{ fontSize: "10px" }}>myCourses</div>
 			</Nav.Link>
-			<Nav.Link href="/history" style={{ flexDirection: "column", textAlign: "center" }}>
-				<img src="../history.png" style={{ width: "40px", height: "40px" }} />
+			<Nav.Link href="/history" className={styles.text}>
+				<img src="../history.png" className={styles.picture} />
 				<div style={{ fontSize: "10px" }}>History</div>
 			</Nav.Link>
-			<Nav.Link href="/cart" style={{ flexDirection: "column", textAlign: "center" }}>
-				<img src="../cart.png" style={{ width: "40px", height: "40px" }} />
+			<Nav.Link href="/cart" className={styles.text}>
+				<img src="../cart.png" className={styles.picture} />
 				<div style={{ fontSize: "10px" }}>myCart</div>
 			</Nav.Link>
 			<Nav.Link
 				onClick={context.logout}
 				href="/"
-				style={{ flexDirection: "column", textAlign: "center" }}>
-				<img src="../box-arrow-right.svg" style={{ width: "40px", height: "40px" }} />
+				className={styles.text}>
+				<img src="../box-arrow-right.svg" className={styles.picture} />
 				<div style={{ fontSize: "10px" }}>Log out</div>
 			</Nav.Link>
-			<Nav.Link href="/" style={{ flexDirection: "column", textAlign: "center" }}>
-				<img src="../chevron-left.png" style={{ width: "40px", height: "40px" }} />
+			<Nav.Link href="/" className={styles.text}>
+				<img src="../chevron-left.png" className={styles.picture} />
 				<div style={{ fontSize: "10px" }}>Back</div>
+			</Nav.Link>
+			<Nav.Link href="/profile" className={styles.text}>
+				<img src="../Profiledf.jpg" className={styles.profile} />
+				<div style={{ fontSize: "10px" }}>profile</div>
 			</Nav.Link>
 		</Nav>
 	);
