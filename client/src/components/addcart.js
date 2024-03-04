61baaee345017b2a895cb8e96d5236b5eb09d68d
Chaithanan <chaithanan.pk5199@gmail.com>
2024-03-04 22:23:23 +0700

    cart improve

--- client/src/components/addcart.js
+++ client/src/components/addcart.js
@@ -8,19 +8,19 @@ import { AuthContext } from "../context/AuthContext";
 function AddCart({ course, onResponse }) {
     const context = useContext(AuthContext);
     const login = context.state.isLoggedIn;
-    const courseContent = course.attributes;
-    const [cart, setcart] = useState(courseContent.entries?.data[0]?.attributes?.cart);
-    const enroll = courseContent.entries?.data[0]?.attributes?.enroll;
+    const courseContent = course.attributes?course.attributes:null;
+    const [cart, setcart] = useState(courseContent?courseContent.entries?.data[0]?.attributes?.cart:course.cart);
+    const enroll = courseContent?courseContent.entries?.data[0]?.attributes?.enroll:course.enroll;
 
     const addToCart = async () => {
         if (login) {
-            const response = await ax.get(conf.apiUrlPrefix + `/courses/${course.id}/toCart`);
+            const response = await ax.get(conf.apiUrlPrefix + `/courses/${courseContent?course.id:course.course.id}/toCart`);
+            setcart(response.data.AddToCart);
             if (onResponse) {
                 onResponse(response);
-            }
-            setcart(response.data.AddToCart);
-        } else {
-        }
+            } else {
+            window.location.reload();}
+        } 
     };
 
     return (
