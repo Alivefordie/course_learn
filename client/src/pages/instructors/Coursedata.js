e10b8b2b5c2e0af4b6bcef9c399762ab14d6f181
Chaithanan <chaithanan.pk5199@gmail.com>
2024-03-06 05:27:12 +0700

    demo course for ins

--- client/src/pages/instructors/Coursedata.js
+++ client/src/pages/instructors/Coursedata.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../../App.css";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Accordion, Card, Button } from "react-bootstrap";
 import NavbarTop from "../../components/NavbarTop";
 import Spinner from "../../components/Spinner";
 import ax from "../../conf/ax";
@@ -10,13 +10,15 @@ import { useParams } from "react-router-dom";
 const Coursedata = () => {
 	const [myData, setMyData] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [CourseSyllabus, setCourseSyllabus] = useState([]);
 	const param = useParams();
 
 	const fetchData = async () => {
 		try {
 			const response = await ax.get(`${conf.apiUrlPrefix}/courses/${param.course}`);
 			setMyData(response.data.data);
-			console.log(response.data.data);
+			console.log('response', response.data.data);
+			setCourseSyllabus(response.data.data.attributes.course_syllabus);
 			setLoading(false);
 		} catch (err) {
 			console.log(err);
@@ -27,10 +29,67 @@ const Coursedata = () => {
 	useEffect(() => {
 		fetchData();
 	}, []);
+	
 	return (
 		<div className="body">
 			<NavbarTop />
-			{loading ? <Spinner /> : <Container></Container>}
+			{loading ? <Spinner /> :
+				<Container className="course-info">
+					<Row>
+							<Col md={6}>
+								<div>
+									<div className="course-title">
+										<img
+											src={conf.url +  myData.attributes.picture.data.attributes.url}
+											alt="item"
+											width={300}
+										/>
+										<span style={{ fontFamily: 'Arial, sans-serif', marginTop: '10px' }}>
+											remaining: {myData.attributes.maxCapacity - myData.attributes.amount}/{myData.attributes.maxCapacity} |  {myData.attributes.likeCount} like<br />
+											<div className="centeredContent" style={{ flexDirection: "column", textAlign: "center", marginTop: "30px", marginBottom: "-50px" }}>
+												<img src="../clock.png" alt="remaining" style={{ width: '40px', height: '40px' }} />
+												<p>ชั่วโมงเรียน {myData.attributes.maxCapacity} ชม.</p>
+											</div>
+										</span >
+
+									</div>
+								</div>
+							</Col>
+							<Col md={6} style={{ marginTop: '20px' }}>
+
+								<div style={{ margin: '20px' }}>
+									<h4 style={{ fontFamily: 'Arial, sans-serif', marginBottom: '8px' }}>{myData.attributes.title}  (Demo)</h4>
+									<p style={{ fontFamily: 'Arial, sans-serif', marginBottom: '4px', color: 'red', fontWeight: 'bold' }}>{myData.attributes.price} ฿</p>
+									<Accordion>
+										<Accordion.Header
+											style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+											<span style={{ marginRight: "auto" }}>Video Preview</span>
+										</Accordion.Header>
+											<Accordion.Body id="course-review">
+												{CourseSyllabus.map((val, index) => {
+													switch (val.__component) {
+														case "activity.review":
+															return (
+																<div key={index}>
+																	<video controls width="250">
+																		<source src={conf.url + val.preview.data.attributes.url} type="video/mp4" />
+																		Your browser does not support the video tag.
+																	</video>
+																</div>
+															);
+													}
+												})}
+											</Accordion.Body>
+									</Accordion>
+									<p style={{ fontFamily: 'Arial, sans-serif', marginBottom: '4px', wordWrap: "break-word", lineHeight: '2' }}>
+										<span style={{ fontStyle: 'italic', textDecoration: 'underline' }}>รายละเอียดคอร์ส</span> : {myData.attributes.description}
+									</p>
+
+								</div>
+
+							</Col>
+						</Row>
+				</Container>}
 		</div>
 	);
 };
