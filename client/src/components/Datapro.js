7d9f9f9b880f641a40f174c287c372787e78d22b
Jukra <jukrachai3146@gmail.com>
2024-03-05 17:50:38 +0700

    profile ui lastly

--- client/src/components/Datapro.js
+++ client/src/components/Datapro.js
@@ -6,7 +6,9 @@ import { Col, Container, Row, Button } from "react-bootstrap";
 const Datapro = ({ data }) => {
   const data_1 = data[0];
   const entries = data[1];
-  const picture = data[3] ? `${conf.url}${data[3]}` : "../../public/Profiledf.jpg";
+  const picture = data[3]
+    ? `${conf.url}${data[3]}`
+    : "../../public/Profiledf.jpg";
   const id = data[4];
 
   useEffect(() => {
@@ -24,20 +26,65 @@ const Datapro = ({ data }) => {
     <div>
       <Container className="profile-con">
         <Row sm="3" md="4" className="profile-row">
-          <Col className="profile-menu" style={{width: "250px"}}>
+          <Col className="profile-menu" style={{ width: "250px" }}>
             <Container className="option-con">
-              <p>picture</p>
-              <p>username</p>
-              <p>email</p>
-              <Button variant="dark">
-                Edit Profile
-              </Button>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <img
+                  src={picture}
+                  alt="Profile"
+                  style={{
+                    width: "150px",
+                    height: "150px",
+                    borderRadius: "50%",
+                    marginBottom: "20px",
+                    boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
+                  }}
+                />
+              </div>
+              <p
+                style={{
+                  fontFamily: "Roboto, sans-serif",
+                  fontSize: "16px",
+                  marginBottom: "10px",
+                }}
+              >
+                <strong>Username:</strong> {data_1.username}
+              </p>
+              <p
+                style={{
+                  fontFamily: "Roboto, sans-serif",
+                  fontSize: "16px",
+                  marginBottom: "10px",
+                }}
+              >
+                <strong>Email:</strong> {data_1.email}
+              </p>
+              <Link
+                to={`./edit-profile/${id}`}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <p
+                  style={{
+                    fontFamily: "Roboto, sans-serif",
+                    fontSize: "16px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  <Button variant="outline-light">Edit Profile</Button>
+                </p>
+              </Link>
             </Container>
           </Col>
-          <Col className="profile-info-col" style={{width: "600px"}}>
+          <Col className="profile-info-col" style={{ width: "600px" }}>
             <Row className="info-row">
               <Col className="profile-info">
-                <h>Profile Information</h>
+                <h>Personal Information</h>
               </Col>
               <div class="w-100"></div>
               <Col className="mycourse-col">
@@ -45,8 +92,8 @@ const Datapro = ({ data }) => {
               </Col>
             </Row>
           </Col>
-          <Col className="my-favorite" style={{width: "350px"}}>
-          <h>myFavorite Course</h>
+          <Col className="my-favorite" style={{ width: "350px" }}>
+            <h>myFavorite Course</h>
           </Col>
         </Row>
       </Container>
