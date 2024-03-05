26af588e2ce6f571c5bc5d572d08da415c60dcc4
Jukra <jukrachai3146@gmail.com>
2024-03-05 14:26:45 +0700

    confused

--- client/src/pages/myCoursesStudent.js
+++ client/src/pages/myCoursesStudent.js
@@ -16,13 +16,47 @@ import fetchOwnerCourses from "../components/Owner";
 import ax from "../conf/ax";
 import conf from "../conf/main";
 import { Link } from "react-router-dom";
+import styles from "../css/Student.module.css";
 
 
 const MyCoursesStudent = () => {
     return(
-        <div>
-            <NavbarTop NavbarLink={NavbarLink}/>
-            
+        <div className = {styles.Bgcolor}>
+            <NavbarTop NavbarLink={NavbarLink} />
+            <Container class="container" > 
+            <Row>
+                      <Col  >
+                        <div class="container p-3 my-3 bg-white text-dark" style = {{fontFamily: '"Righteous", sans-serif' , borderRadius : "15px" , boxShadow: "2px 2px 8px 4px rgba(0, 0, 0, 0.3)"}}>Study 📚 ✏️</div>
+                        <div class="container p-2 my-3 bg-white text-dark" style = {{fontFamily: '"Righteous", sans-serif',borderRadius : "15px"  ,boxShadow: "2px 2px 8px 4px rgba(0, 0, 0, 0.3)"}}>
+                            Video 🎬
+                            <div>
+                                <video width="auto" height="auto" controls>
+                                    <source src="movie.mp4" type="video/mp4"/>
+                                    <source src="movie.ogg" type="video/ogg"/>
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        </div>
+                     </Col>
+                     <Col>
+                        <div class="container p-3 my-3 bg-white text-dark" style = {{fontFamily: '"Righteous", sans-serif',boxShadow: "2px 2px 8px 4px rgba(0, 0, 0, 0.3)"}}>
+                            mycourses 💼
+                        </div>
+                        <div class="container p-3 my-3 bg-white text-dark" style = {{fontFamily: '"Righteous", sans-serif',boxShadow: "2px 2px 8px 4px rgba(0, 0, 0, 0.3)"}}>
+                            <Card >
+                               
+                                <Card.Body>
+                                    <Card.Title >Display Course</Card.Title>
+                                    <Card.Text style = {{marginTop: "6vw"}}>
+                                    Study Progression --🕙--
+                                    </Card.Text>
+                                    <Link to="/mycourses" class="btn btn-dark">See Progression 👀</Link>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                     </Col>      
+            </Row>
+            </Container>
         </div>
     );
 };
