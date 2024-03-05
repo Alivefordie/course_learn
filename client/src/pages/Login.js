6d913f3ab46fc67a04557b24e189cf47fd4c0555
Jukra <jukrachai3146@gmail.com>
2024-03-05 10:25:38 +0700

    fix login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,9 @@
-import React, { useState, useContext } from "react";
-import { Form, Button, Container, Col } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Button, Col, Container, Form, Spinner } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import NavbarTop from "../components/NavbarTop";
 import { AuthContext } from "../context/AuthContext";
+import NavbarTop from "../components/NavbarTop";
 import styles from "../css/LoginCss.module.css";
-import Spinner from "../components/Spinner";
 
 const Login = () => {
   const { login } = useContext(AuthContext);
@@ -42,70 +41,67 @@ const Login = () => {
               />
             </Container>
             <Form onSubmit={handleSubmit} className={styles.username}>
-              <Form onSubmit={handleSubmit} className={styles.username}>
-                <Form.Group controlId="username">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter Your Email"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                    className={styles.resize}
-                  />
-                </Form.Group>
-                <Form.Group controlId="password">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Enter Your Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    className={styles.resize}
-                  />
-                </Form.Group>
-                <div>
-                  <br />
-                </div>
-                <div className="d-flex justify-content-between">
-                  <span
-                    className=" d-flex "
-                    onClick={() => navigate("/ForgotPassword")}
-                    style={{
-                      textDecorationLine: "underline",
-                      cursor: "pointer",
-                    }}
-                  >
-                    Forgot Password
-                  </span>
-                  <span
-                    onClick={() => navigate("/register")}
-                    style={{
-                      textDecorationLine: "underline",
-                      cursor: "pointer",
-                    }}
-                  >
-                    Don't have Account?
-                  </span>
-                </div>
-                <div className="text-center">
-                  <Button
-                    variant="light"
-                    type="submit"
-                    className=" mt-3"
-                    style={{
-                      justifyContent: "center",
-                      fontWeight: "bold",
-                      borderRadius: "15px",
-                      width: "17vw",
-                    }}
-                  >
-                    Login
-                  </Button>
-                </div>
-              </Form>
-              ;
+              <Form.Group controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Your Email"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  className={styles.resize}
+                />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter Your Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className={styles.resize}
+                />
+              </Form.Group>
+              <div>
+                <br />
+              </div>
+              <div className="d-flex justify-content-between">
+                <span
+                  className=" d-flex "
+                  onClick={() => navigate("/ForgotPassword")}
+                  style={{
+                    textDecorationLine: "underline",
+                    cursor: "pointer",
+                  }}
+                >
+                  Forgot Password
+                </span>
+                <span
+                  onClick={() => navigate("/register")}
+                  style={{
+                    textDecorationLine: "underline",
+                    cursor: "pointer",
+                  }}
+                >
+                  Don't have Account?
+                </span>
+              </div>
+              <div className="text-center">
+                <Button
+                  variant="light"
+                  type="submit"
+                  className=" mt-3"
+                  style={{
+                    justifyContent: "center",
+                    fontWeight: "bold",
+                    borderRadius: "15px",
+                    width: "17vw",
+                  }}
+                >
+                  Login
+                </Button>
+              </div>
             </Form>
           </Col>
           <Col className={styles.loginImageContainer}>
