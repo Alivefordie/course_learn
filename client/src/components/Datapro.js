a895227cea3e45c7e20c130a82c9be60d3ebc331
Jukra <jukrachai3146@gmail.com>
2024-03-05 17:26:36 +0700

    UI profie new draft

--- client/src/components/Datapro.js
+++ client/src/components/Datapro.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import conf from "../conf/main";
+import { Col, Container, Row, Button } from "react-bootstrap";
 
 const Datapro = ({ data }) => {
   const data_1 = data[0];
@@ -20,106 +21,35 @@ const Datapro = ({ data }) => {
   console.log("filteredEntries:", filteredEntries);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: "#f9f9f9",
-          width: "80%",
-		  height: "100vh",
-          padding: "20px",
-          borderRadius: "10px",
-          boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        <div>
-          <h2
-            style={{
-              fontFamily: "Roboto, sans-serif",
-              fontSize: "24px",
-              color: "#333",
-              borderBottom: "2px solid #ccc",
-              paddingBottom: "10px",
-              marginBottom: "20px",
-			  textAlign: "center"
-            }}
-          >
-            Profile Information
-          </h2>
-          <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
-            <img
-              src={picture}
-              alt="Profile"
-              style={{
-                width: "150px",
-                height: "150px",
-                borderRadius: "50%",
-                marginBottom: "20px",
-                boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
-              }}
-            />
-            <div style={{ textAlign: "center" }}>
-              <p style={{ fontFamily: "Roboto, sans-serif", fontSize: "16px", marginBottom: "10px" }}>
-                <strong>Username:</strong> {data_1.username}
-              </p>
-              <p style={{ fontFamily: "Roboto, sans-serif", fontSize: "16px", marginBottom: "10px" }}>
-                <strong>Email:</strong> {data_1.email}
-              </p>
-              <Link
-                to={`./edit-profile/${id}`}
-                style={{ textDecoration: "none", color: "black" }}
-              >
-                <p style={{ fontFamily: "Roboto, sans-serif", fontSize: "16px", marginBottom: "10px" }}>
-                  <strong>Edit Profile</strong>
-                </p>
-              </Link>
-            </div>
-          </div>
-          <div
-            style={{
-              backgroundColor: "#fff",
-              borderRadius: "5px",
-              padding: "20px",
-              boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
-            }}
-          >
-            <p
-              style={{
-                fontFamily: "Roboto, sans-serif",
-                fontSize: "18px",
-                marginBottom: "20px",
-                color: "#333",
-              }}
-            >
-              <strong>My Course Details:</strong>
-            </p>
-            <ul style={{ padding: "0", margin: "0" }}>
-              {filteredEntries.map((entry, index) => (
-                <li
-                  key={index}
-                  style={{
-                    fontFamily: "Roboto, sans-serif",
-                    fontSize: "16px",
-                    marginBottom: "10px",
-                    listStyleType: "none",
-                  }}
-                >
-                  <Link to="#" className="custom-link">
-                    <strong>{entry.course.title}</strong>
-                    <br />- {entry.course.description}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Container className="profile-con">
+        <Row sm="3" md="4" className="profile-row">
+          <Col className="profile-menu" style={{width: "250px"}}>
+            <Container className="option-con">
+              <p>picture</p>
+              <p>username</p>
+              <p>email</p>
+              <Button variant="dark">
+                Edit Profile
+              </Button>
+            </Container>
+          </Col>
+          <Col className="profile-info-col" style={{width: "600px"}}>
+            <Row className="info-row">
+              <Col className="profile-info">
+                <h>Profile Information</h>
+              </Col>
+              <div class="w-100"></div>
+              <Col className="mycourse-col">
+                <h>myCourse</h>
+              </Col>
+            </Row>
+          </Col>
+          <Col className="my-favorite" style={{width: "350px"}}>
+          <h>myFavorite Course</h>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
