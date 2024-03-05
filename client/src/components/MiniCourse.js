677231bf50537a30ce068afe8ad3f2af11177ba5
Kittanawat Wattanakit <kittanawat0916@gmail.com>
2024-03-05 09:37:52 +0700

    Code-Conflict

--- client/src/components/MiniCourse.js
+++ client/src/components/MiniCourse.js
@@ -24,73 +24,7 @@ function MiniCourse({ course }) {
 	}, []);
 
 	return (
-		<>
-<<<<<<< HEAD
-			<Card className="d-flex flex-row" style = {{marginTop : '15px',marginRight: '5px'}}>
-				<div
-					onClick={() => navigate(`/courses/${course.id}`, { replace: true })}
-					style={{ cursor: "pointer" }}
-					className="image-col">
-					<Card.Img
-						className="course-image"
-						variant="left"
-						src={"http://localhost:1337" + picture}
-					/>
-				</div>
-				<div className="body-col">
-					<Card.Body
-						onClick={() => navigate(`/courses/${course.id}`, { replace: true })}
-						style={{ cursor: "pointer" }}>
-						<Card.Title>{courseContent.title}✅</Card.Title>
-						<Card.Text>details</Card.Text>
-						<Card.Text className="m-0">เนื้อหา {courseContent.description}</Card.Text>
-						<Card.Text className="m-0">ระยะเวลา {courseContent.duration}</Card.Text>
-						<Card.Text className="m-0">ครู {teacher.username}</Card.Text>
-					</Card.Body>
-					<Col className="add-icon position-absolute top-0 end-0 p-2">
-						{enroll ? (
-							<img
-								onClick={addToCart}
-								style={{ cursor: "pointer", width: "100%", width: "20px", height: "20px" }}
-								src="../check-all.svg"
-								alt="Add Icon"
-							/>
-						) : cart ? (
-							<img
-								onClick={addToCart}
-								style={{ cursor: "pointer", width: "100%", width: "20px", height: "20px" }}
-								src="../cart-check.svg"
-								alt="Add Icon"
-							/>
-						) : (
-							<img
-								onClick={addToCart}
-								style={{ cursor: "pointer", width: "100%", width: "20px", height: "20px" }}
-								src="../plus.png"
-								alt="Add Icon"
-							/>
-						)}
-					</Col>
-					<Col className="heart-icon position-absolute bottom-0 end-0 p-2">
-						{!like ? (
-							<img
-								onClick={addTolike}
-								style={{ cursor: "pointer", width: "100%", width: "20px", height: "20px" }}
-								src="../heart.png"
-								alt="Like Icon"
-							/>
-						) : (
-							<img
-								onClick={addTolike}
-								style={{ cursor: "pointer", width: "100%", width: "20px", height: "20px" }}
-								src="../red-heart-icon.svg"
-								alt="Like Icon"
-							/>
-						)}
-					</Col>
-				</div>
-			</Card>
-=======
+		<div>
 			<Card className="d-flex flex-row" style={{ marginTop: "15px" }}>
             <div
                 onClick={() => navigate(`/courses/${course.id}`)}
@@ -116,8 +50,7 @@ function MiniCourse({ course }) {
                 </Col>
             </div>
         </Card>
->>>>>>> 20c07217a18b7d3fd9f89c3b8f235d8f4aa6c53b
-		</>
+		</div>
 	);
 }
 export default MiniCourse;
--- client/src/pages/myCoursesStudent.js
+++ client/src/pages/myCoursesStudent.js
@@ -12,11 +12,15 @@ import NavbarLink from "../components/NavbarLink";
 import "../App.css";
 import Spinner from "../components/Spinner";
 import fetchOwnerCourses from "../components/Owner";
+import ax from "../conf/ax";
+import conf from "../conf/main";
+import { Link } from "react-router-dom";
+
 
 const MyCoursesStudent = () => {
     return(
         <div>
-            
+            <NavbarLink/>
             
         </div>
     );
